e78f48b7d97d70f17e6bb2ca778b4ad84b395be2
Keikoooo <41388047+Keikoooo@users.noreply.github.com>
2018-07-19 02:34:56 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -9,16 +9,6 @@ client.on('ready', () => {
 });
 
 client.on('message', message => {
-    if (message.author.bot || message.content.startsWith(prefix)) return;
-    const args = message.content.slice(prefix.length).split(" ")
-    const command = args.shift().toLowercase();
-    
-    if (command === 'ping') {
-        const then = Date.now();
-        message.channel.send('pinging...').then(>> => {
-        >>.edit('Pong! It took ${Date.now - then}ms!\nDiscord Heartbeart:${bot.ping}');
-        });
-    }
 });
 
 // THIS  MUST  BE  THIS  WAY
