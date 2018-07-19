1322ec2dc8974539674c3acf95fbf158731f8cf9
Keikoooo <41388047+Keikoooo@users.noreply.github.com>
2018-07-19 00:01:28 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -7,10 +7,20 @@ client.on('ready', () => {
     client.user.setActivity(`with Keiko!`);
 });
 
-client.on('message', message => {
-    if (message.content === 'ping') {
-    	message.reply('pong');
-  	}
+client.on('message', async message => {
+    if(message.author.bot) return;
+    if(message.content.indexOf(config.prefix) !== 0) return;
+    
+    const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
+    const command = args.shift().toLowerCase();
+    
+    if(command === "ping") {
+    // Calculates ping between sending a message and editing it, giving a nice round-trip latency.
+    // The second ping is an average latency between the bot and the websocket server (one-way, not round-trip)
+    const m = await message.channel.send("Ping?");
+    m.edit(`Pong! Latency is ${m.createdTimestamp - message.createdTimestamp}ms. API Latency is ${Math.round(client.ping)}ms`);
+    }
+    
     if (message.content === 'pong') {
     	message.reply('ping');
   	}
