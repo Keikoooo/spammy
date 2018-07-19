68a2e5fb1709f1b348b82a1f265fdf71c177d8c0
Keikoooo <41388047+Keikoooo@users.noreply.github.com>
2018-07-19 00:09:19 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,13 +1,13 @@
-const Discord = require('discord.js');
+const Discord = require("discord.js");
 const client = new Discord.Client();
-const config = require('./config.json');
+const config = require("./config.json");
 
-client.on('ready', () => {
+client.on("ready", () => {
     console.log(`I am ready!`);
     client.user.setActivity(`with Keiko!`);
 });
 
-client.on('message', async message => {
+client.on("message", async message => {
     if(message.author.bot) return;
     if(message.content.indexOf(config.prefix) !== 0) return;
     const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
@@ -18,8 +18,8 @@ client.on('message', async message => {
     m.edit(`Pong! Latency is ${m.createdTimestamp - message.createdTimestamp}ms. API Latency is ${Math.round(client.ping)}ms`);
     }
     
-    if (message.content === 'pong') {
-    	message.reply('ping');
+    if (message.content === "pong") {
+    	message.reply("ping");
   	}
 });
 
