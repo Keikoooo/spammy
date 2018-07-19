e1a87d8633288b085344670efcea05e2d10fdc6f
Keikoooo <41388047+Keikoooo@users.noreply.github.com>
2018-07-19 00:23:02 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,13 +1,13 @@
-const Discord = require("discord.js");
+const Discord = require('discord.js');
 const client = new Discord.Client();
-const config = require("./config.json");
+const prefix = 'fel ';
 
-client.on("ready", () => {
+client.on('ready', () => {
     console.log(`I am ready!`);
     client.user.setActivity(`with Keiko!`);
 });
 
-client.on("message", async message => {
+client.on('message', async message => {
     if(message.author.bot) return;
     if(message.content.indexOf(config.prefix) !== 0) return;
     const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
