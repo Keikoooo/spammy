3c8267cdf8c50927d8f8213d9fa6de9f9325c94b
Keikoooo <41388047+Keikoooo@users.noreply.github.com>
2018-07-19 10:21:45 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,21 +1,23 @@
 const Discord = require('discord.js');
 const request = require('request');
 const client = new Discord.Client();
-const prefix = '=';
+const prefix = '>>';
 
 client.on('ready', () => {
     console.log(`I am ready!`);
     client.user.setPresence({ game: { name: `with Keiko!`, type: 0 } });
 });
 
-client.on('message', function(msg) => {
-    if (msg.author.bot) {
-        return;
-    }
-    if(msg.content === prefix + "ping") {
-        msg.channel.send(`Pinging...`).then(function(m) {
-            m.edit(`Pong! The message took ${m.createTimestamp - msg.createdTimestamp}ms!`)
-    })
+client.on('message', (message) => {
+  // Exit and stop if it's not there
+  if (!message.content.startsWith(prefix)) return;
+
+  if (message.content.startsWith(prefix + "ping")) {
+    message.channel.send("Pong!");
+  } else
+  if (message.content.startsWith(prefix + "pong")) {
+    message.channel.send("ping!");
+  }
 });
 
 // THIS  MUST  BE  THIS  WAY
