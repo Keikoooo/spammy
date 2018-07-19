0e24a207ceef83932469fa5c98dc0e4e592d3c8b
Keikoooo <41388047+Keikoooo@users.noreply.github.com>
2018-07-18 23:21:17 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,8 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
+const botconfig = require('./botconfig.json');
+
 client.on('ready', () => {
     console.log('I am ready!');
 });
