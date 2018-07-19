52d6f0002e7b12488b7580b0754b6fb91bf70806
Keikoooo <41388047+Keikoooo@users.noreply.github.com>
2018-07-19 00:54:48 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -21,12 +21,8 @@ client.on('message', message => {
     args[0] = args[0].toLowerCase();
 
     if (command === 'ping') {
-        message.channel.sendMessage("Pong!");
-    } else
-
-    // const m = await message.channel.send("Ping?");
-    //m.edit(`Pong! Latency is ${m.createdTimestamp - message.createdTimestamp}ms. API Latency is ${Math.round(client.ping)}ms`);
-    }
+        message.channel.sendMessage(`Pong! Latency is ${m.createdTimestamp - message.createdTimestamp}ms. API Latency is ${Math.round(client.ping)}ms`);
+    } else  
 });
 
 // THIS  MUST  BE  THIS  WAY
