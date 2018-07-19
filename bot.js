ad475496e4fcf8c021b80b2800b168aaf6408818
Keikoooo <41388047+Keikoooo@users.noreply.github.com>
2018-07-18 23:57:08 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,7 +3,7 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 
 client.on('ready', () => {
-    console.log(`${bot.user.username} is online!`);
+    console.log(I am ready!`);
     client.user.setActivity(`with Keiko!`);
 });
 
