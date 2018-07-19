6c86a9175447ce346aaa9e09a8852ac071a2a1e0
Keikoooo <41388047+Keikoooo@users.noreply.github.com>
2018-07-19 00:06:54 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -10,13 +10,10 @@ client.on('ready', () => {
 client.on('message', async message => {
     if(message.author.bot) return;
     if(message.content.indexOf(config.prefix) !== 0) return;
-    
     const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
     const command = args.shift().toLowerCase();
     
     if(command === "ping") {
-    // Calculates ping between sending a message and editing it, giving a nice round-trip latency.
-    // The second ping is an average latency between the bot and the websocket server (one-way, not round-trip)
     const m = await message.channel.send("Ping?");
     m.edit(`Pong! Latency is ${m.createdTimestamp - message.createdTimestamp}ms. API Latency is ${Math.round(client.ping)}ms`);
     }
