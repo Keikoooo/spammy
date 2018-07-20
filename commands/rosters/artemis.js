0121b7a0a556c12485da43c52de1b48c8568344a
Keiko Yukimura <41388047+Keikoooo@users.noreply.github.com>
2018-07-20 18:59:21 -0400

    Create artemis.js

---
+++ commands/rosters/artemis.js
@@ -0,0 +1,7 @@
+exports.run = (client, message, args) => {
+    message.channel.send({embed: {
+    color: 12887295,
+    title: "Roster for Artemis",
+    description: "mililu\nSnakeHenry"
+    }});
+}
