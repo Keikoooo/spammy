72240fc06b849b7094bef5c5afe961aa3a1e1b91
Keiko Yukimura <41388047+Keikoooo@users.noreply.github.com>
2018-07-20 19:07:27 -0400

    Update roster.js

--- commands/roster.js
+++ commands/roster.js
@@ -1,8 +1,4 @@
 exports.run = (client, message, args) => {
-  try {
-    let rosterFile= require(`./rosters/${roster}.js`);
-    rosterFile.run(client, message, args);
-  } catch (err) {
-    console.error(err);
-  }
+ try {
+    let commandFile = require(`./rosters/${roster}.js`);
 }
