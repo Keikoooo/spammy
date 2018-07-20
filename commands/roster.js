04fd71aa534df660e60c135d9cbb2522d5a8b960
Keiko Yukimura <41388047+Keikoooo@users.noreply.github.com>
2018-07-20 19:59:23 -0400

    Update roster.js

--- commands/roster.js
+++ commands/roster.js
@@ -1,26 +1,26 @@
 exports.run = (client, message, args) => {
-    if (args = 'aero') {
+    if (args == 'aero') {
     message.channel.send({embed: {
     color: 12887295,
     title: "Roster for Aero",
     description: "Rank 7\n\nohjustame\nSilverSky009\n111titanic223\nanvae\nAffectings\nAnnaby\nAurrem\nChrisYFS\nendersama\nFTZero3o\nKuroSmh\nBoysLikeYou\nPignite\nTheMLGDucky\nTzipporah\nvaliare\nxLeaqh\nInevoked\nCiaara"
     }});
   } else
-    if (args = 'artemis') {
+    if (args == 'artemis') {
     message.channel.send({embed: {
     color: 12887295,
     title: "Roster for Artemis",
     description: "Rank 5\n\nmililu\nSnakeHenry\nskkketchy"
     }});
   } else
-    if (args = 'echo') {
+    if (args == 'echo') {
     message.channel.send({embed: {
     color: 12887295,
     title: "Roster for Echo",
     description: "Rank 1\n\ycnan\naayanna"
     }});
   } else
-    if (args = 'mcthicc') {
+    if (args == 'mcthicc') {
     message.channel.send({embed: {
     color: 12887295,
     title: "Roster for MCTHICC",
