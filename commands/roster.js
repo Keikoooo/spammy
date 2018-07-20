4d30f13c0c02d0ac57ab3f4b4c8d012d3314bace
Keiko Yukimura <41388047+Keikoooo@users.noreply.github.com>
2018-07-20 19:00:11 -0400

    Update roster.js

--- commands/roster.js
+++ commands/roster.js
@@ -1,7 +1,11 @@
 exports.run = (client, message, args) => {
+  try {
+    let rosterFile= require(`./rosters/${roster}.js`);
+    rosterFile.run(client, message, args);
+    
     message.channel.send({embed: {
     color: 12887295,
-    title: "Roster for "Team"",
-    description: "Working on it later"
+    title: "Roster for ${rosters}",
+    description: "./rosters/${rosters}.js"
     }});
 }
