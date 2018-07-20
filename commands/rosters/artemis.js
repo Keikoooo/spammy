0125d9e921f80d015084397118ce045b4bd880a4
Keiko Yukimura <41388047+Keikoooo@users.noreply.github.com>
2018-07-20 19:31:31 -0400

    Delete artemis.js

--- commands/rosters/artemis.js
+++
@@ -1,7 +0,0 @@
-exports.run = (client, message, args) => {
-    message.channel.send({embed: {
-    color: 12887295,
-    title: "Roster for Artemis",
-    description: "Rank 5\n\nmililu\nSnakeHenry"
-    }});
-}
