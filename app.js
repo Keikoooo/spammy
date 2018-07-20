269ba3d0726ed43b54739574bea426a46b851ea9
Keiko Yukimura <41388047+Keikoooo@users.noreply.github.com>
2018-07-20 11:27:34 -0400

    Update app.js

--- app.js
+++ app.js
@@ -10,27 +10,31 @@ client.on('ready', () => {
     client.user.setPresence({ game: { name: `with Keiko!`, type: 0 } });
 });
 
+// This loop reads the /events/ folder and attaches each event file to the appropriate event.
+fs.readdir("./events/", (err, files) => {
+  if (err) return console.error(err);
+  files.forEach(file => {
+    let eventFunction = require(`./events/${file}`);
+    let eventName = file.split(".")[0];
+    // super-secret recipe to call events with all their proper arguments *after* the `client` var.
+    client.on(eventName, (...args) => eventFunction.run(client, ...args));
+  });
+});
+
 client.on('message', (message) => {
   // Exit and stop if the prefix is not there or if user is a bot
   if (!message.content.startsWith(config.prefix) || message.author.bot) return;;
   const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
   const command = args.shift().toLowerCase();
 
-  switch (command) {
-  case "ping" :
-    message.channel.send('Pong!');
-    break;
-  case "blah" :
-    message.channel.send('Meh.');
-    break;
-  case "keiko" :
-    message.channel.send({embed: {
-    color: 12887295,
-    title: "Information for Keiko",
-    description: "There are no messages yet."
-    }});
-    break;      
-    }
+    
+  // The list of if/else is replaced with those simple 2 lines:
+  try {
+    let commandFile = require(`./commands/${command}.js`);
+    commandFile.run(client, message, args);
+  } catch (err) {
+    console.error(err);
+  }
 });
 
 // THIS  MUST  BE  THIS  WAY
