c803870d800bc00460483267ea66d4bdd8d6c969
Keiko Yukimura <41388047+Keikoooo@users.noreply.github.com>
2018-07-20 15:53:14 -0400

    Update reload.js

--- commands/reload.js
+++ commands/reload.js
@@ -1,6 +1,5 @@
-exports.run = (client, message, args) => {
-  if(!args || args.size < 1) return message.reply("Must provide a command name to reload.");
-  // the path is relative to the *current folder*, so just ./filename.js
-  delete require.cache[require.resolve(`./${args[0]}.js`)];
-  message.reply(`The command ${args[0]} has been reloaded`);
+const main = require('../app.js');
+exports.run = function (client, message, args) => {
+  let cmd = args.join(' ');
+  main.reload(message, cmd);
 };
