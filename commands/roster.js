35cf793924d321a2272c691d0abb2a46f2711061
Keiko Yukimura <41388047+Keikoooo@users.noreply.github.com>
2018-07-20 15:21:06 -0400

    Create roster.js

---
+++ commands/roster.js
@@ -0,0 +1,7 @@
+exports.run = (client, message, args) => {
+    message.channel.send({embed: {
+    color: 12887295,
+    title: "Roster for "Team"",
+    description: "Working on it later"
+    }});
+}
