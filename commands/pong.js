026723dd0fb74eb821bbc5641fec5b11f6729577
Keiko Yukimura <41388047+Keikoooo@users.noreply.github.com>
2018-07-20 15:15:40 -0400

    Update pong.js

--- commands/pong.js
+++ commands/pong.js
@@ -1,3 +1,3 @@
 exports.run = (client, message, args) => {
-    message.channel.send(`Ping! Please do \fel ping\ to check ping.`).catch(console.error);
+    message.channel.send(`Ping! Please do \`fel ping\` to check ping.`).catch(console.error);
 }
