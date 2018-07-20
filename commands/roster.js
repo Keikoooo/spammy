185d336b83b9eb8ec70fe11b8ff8257a33175b98
Keiko Yukimura <41388047+Keikoooo@users.noreply.github.com>
2018-07-20 19:53:03 -0400

    Update roster.js

--- commands/roster.js
+++ commands/roster.js
@@ -1,4 +1,11 @@
 exports.run = (client, message, args) => {
+    if (args = 'aero') {
+    message.channel.send({embed: {
+    color: 12887295,
+    title: "Roster for Aero",
+    description: "Rank 7\n\nohjustame\nSilverSky009\n111titanic223\nanvae\nAffectings\nAnnaby\nAurrem\nChrisYFS\nendersama\nFTZero3o\nKuroSmh\nBoysLikeYou\nPignite\nTheMLGDucky\nTzipporah\nvaliare\nxLeaqh\nInevoked\nCiaara"
+    }});
+  } else
     if (args = 'artemis') {
     message.channel.send({embed: {
     color: 12887295,
