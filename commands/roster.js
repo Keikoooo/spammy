138dabf1e5e7722f7a46eda8a8925b70ea37d877
Keiko Yukimura <41388047+Keikoooo@users.noreply.github.com>
2018-07-20 19:31:23 -0400

    Update roster.js

--- commands/roster.js
+++ commands/roster.js
@@ -1,4 +1,16 @@
 exports.run = (client, message, args) => {
-    let rosterFile = require(`../rosters/${roster}.js`);
-    rosterFile.run(client, message, args);
+    if (args === 'artemis') {
+    message.channel.send({embed: {
+    color: 12887295,
+    title: "Roster for Artemis",
+    description: "Rank 5\n\nmililu\nSnakeHenry"
+    }});
+  } else
+    if (args === 'echo') {
+    message.channel.send({embed: {
+    color: 12887295,
+    title: "Roster for Echo",
+    description: "Rank 1\n\ycnan\naayanna"
+    }});
+  } else
 }
