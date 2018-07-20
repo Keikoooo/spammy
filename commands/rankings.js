7dc492e1bbe990d2d0eec5a9aca48a33f24e69d4
Keiko Yukimura <41388047+Keikoooo@users.noreply.github.com>
2018-07-20 13:55:34 -0400

    Create rankings.js

---
+++ commands/rankings.js
@@ -0,0 +1,7 @@
+exports.run = (client, message, args) => {
+    message.channel.send({embed: {
+    color: 12887295,
+    title: "Rankings",
+    description: "1. Echo\n 2. Calamity 3. IDK\n 4. Velocity\n 4. Velocity\n 5. Artemis\n 6. OP\n 7. Aero\n 8. Kystic\n 9. Ascent\n 10. McThicc\n 11. BloodQuill\n 12. Plutonians"
+    }});
+}
