a77919f1b707a7bd9466abb8bc2f5e02395219ca
Keiko Yukimura <41388047+Keikoooo@users.noreply.github.com>
2018-07-21 14:36:09 -0400

    Create padd.js

---
+++ commands/padd.js
@@ -0,0 +1,10 @@
+exports.run = (client, message, args[0], args[1]) => {
+  let rosters = require("./roster.js");
+  let [player, team] = args;
+  
+  if (arg[1]) {
+    rosters.run(client, message, args[1]);
+  } else
+    message.channel.send(`Please insert a team.`);  
+  }
+}
