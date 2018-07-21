823a48f4858428dd5bc3c2221639cd8040df7bf5
Keiko Yukimura <41388047+Keikoooo@users.noreply.github.com>
2018-07-21 14:34:58 -0400

    Update premove.js

--- commands/premove.js
+++ commands/premove.js
@@ -2,7 +2,7 @@ exports.run = (client, message, args[0], args[1]) => {
   let rosters = require("./roster.js");
   let [player, team] = args;
   
-  if (!arg[1]) {
+  if (arg[1]) {
     rosters.run(client, message, args[1]);
   } else
     message.channel.send(`Please insert a team.`);  
