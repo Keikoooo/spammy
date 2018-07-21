f7afed05e6e7a787d8f21d916dff820908b27972
Keiko Yukimura <41388047+Keikoooo@users.noreply.github.com>
2018-07-21 17:28:58 -0400

    Update testing.js

--- commands/testing.js
+++ commands/testing.js
@@ -1,18 +1,18 @@
 exports.run = (client, message, args) => {
     let [add, remove, player, team] = args;
     
-    if (args == 'aero') {     
-    let embed = new Discord.RichEmbed()
-        .setTitle("Roster for Aero")
-        .setColor(12887295)
-        .setDescription("Rank 7\n\n${aeroroster[0]}\n${aeroroster[1]}")
-    message.channel.send({embed});
+    if (args == 'aero') {
+    message.channel.send({embed: {
+    color: 12887295,
+    title: "Roster for Aero",
+    description: "Rank 7\n\n${aeroroster[0]}\n${aeroroster[1]}"
+    }});
   } else
     if (args == 'artemis') {
-    let embed = new Discord.RichEmbed()
-       .setTitle("Roster for Artemis")
-       .setColor(12887295)
-       .setDescription("Rank 5\n\n${aeroroster[0]}\n${aeroroster[1]}")
-    message.channel.send({embed});
+    message.channel.send({embed: {
+    color: 12887295,
+    title: "Roster for Artemis",
+    description: "Rank 5\n\n${aeroroster[0]}\n${aeroroster[1]}"
+    }});
   }
 }
