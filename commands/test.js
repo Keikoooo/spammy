6645bad7db4717e1288ee60720fdf548f7a70d5d
Keiko Yukimura <41388047+Keikoooo@users.noreply.github.com>
2018-07-21 15:00:40 -0400

    Update test.js

--- commands/test.js
+++ commands/test.js
@@ -1,7 +1,7 @@
 exports.run = (client, message, args) => {
   let rosters = require("./roster.js");
   
-  if (args) {
+  if (args == ${args}) {
     rosters.run(client, message, args);
   } else
     message.channel.send(`Please insert a team.`);  
