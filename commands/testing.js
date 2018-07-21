96d28a536cd34a66382861d4047aaec59374d229
Keiko Yukimura <41388047+Keikoooo@users.noreply.github.com>
2018-07-21 17:25:57 -0400

    Update testing.js

--- commands/testing.js
+++ commands/testing.js
@@ -1,21 +1,18 @@
 exports.run = (client, message, args) => {
     let [add, remove, player, team] = args;
-    var player = [0, 1];
-
-    if (args == 'aero') {
-    let [ohjustame, SilverSky009] = player;      
+    
+    if (args == 'aero') {     
     let embed = new Discord.RichEmbed()
         .setTitle("Roster for Aero")
         .setColor(12887295)
-        .setDescription("Rank 7\n\n${player[0]}\n${player[1]}")
+        .setDescription("Rank 7\n\n${aeroroster[0]}\n${aeroroster[1]}")
     message.channel.send({embed});
   } else
     if (args == 'artemis') {
-    let [mililu, SnakeHenry] = player;
     let embed = new Discord.RichEmbed()
        .setTitle("Roster for Artemis")
        .setColor(12887295)
-       .setDescription("Rank 5\n\n${player[0]}\n${player[1]}")
+       .setDescription("Rank 5\n\n${aeroroster[0]}\n${aeroroster[1]}")
     message.channel.send({embed});
   }
 }
