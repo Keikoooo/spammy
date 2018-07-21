cdd228edbfa9da74db125879e9e8ae600f315fdc
Keiko Yukimura <41388047+Keikoooo@users.noreply.github.com>
2018-07-21 11:44:27 -0400

    Update reload.js

--- commands/reload.js
+++ commands/reload.js
@@ -1,11 +1,6 @@
 exports.run = (client, message, args) => {
-  resetBot(message.channel);
-  break;
-  
-  function resetBot(channel) {
-    // send channel a message that you're resetting bot [optional]
-    channel.send('Resetting...')
-    .then(msg => client.destroy())
-    .then(() => client.login(process.env.BOT_TOKEN));
+// Reboot command
+    msg.channel.sendMessage('Restarting...')
+    reboot().catch(console.log);
   }
-};
+}
