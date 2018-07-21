0590219eab820b9f1d8564bfeffe386b5304cd4b
Keiko Yukimura <41388047+Keikoooo@users.noreply.github.com>
2018-07-21 14:04:56 -0400

    Update roster.js

--- commands/roster.js
+++ commands/roster.js
@@ -1,89 +1,89 @@
 exports.run = (client, message, args) => {
-    if (args == 'aero') {
+    if (args === 'aero') {
     message.channel.send({embed: {
     color: 12887295,
     title: "Roster for Aero",
     description: "Rank 7\n\nohjustame\:crown:\nSilverSky009\:crown:\:bow_and_arrow:\n111titanic223\nanvae\nAffectings\nAnnaby\nAurrem\nChrisYFS\:eight_spoked_asterisk:️\nendersama\nFTZero3o\nKuroSmh\nBoysLikeYou\nPignite\nTheMLGDucky\nTzipporah\nvaliare\nxLeaqh\nInevoked\nCiaara\nDeadpurple75"
     }});
   } else
-    if (args == 'artemis') {
+    if (args === 'artemis') {
     message.channel.send({embed: {
     color: 12887295,
     title: "Roster for Artemis",
     description: "Rank 5\n\nmililu\:crown:\nSnakeHenry\:crown:\:scales:️\nEchanz\nFarzine\nguccitaee\nJqshZ3\nTELETUBBYJUSTIN\nkwehwy\nskkketchy\ndereknishimoto\npluv\nReachMafia\nskei\nSwipy\n_4H\n_Cath\n_Livyy_"
     }});
   } else
-    if (args == 'bloodquill') {
+    if (args === 'bloodquill') {
     message.channel.send({embed: {
     color: 12887295,
     title: "Roster for BloodQuill",
     description: "Rank 11\n\nAnaworfis\:crown:\nLuzifeer\:crown:\njeroNV\nfindollo\nkikina01cz\nlunular\nOdran_\nPolygonWater\nXXXTentac1ion"
     }});
   } else
-    if (args == 'calamity') {
+    if (args === 'calamity') {
     message.channel.send({embed: {
     color: 12887295,
     title: "Roster for Calamity",
     description: "Rank 2\n\nCrownedVictor\:crown:\nWackaroni\:crown:\nAlpha_Pup10\nButterxkiller\nCaait\nJessaye\nNoobAcc\nWhatTheNut\nxGanny\nXV1etnam\nZephyrrStorm\n_Mentals\n__Double"
     }});
   } else
-    if (args == 'echo') {
+    if (args === 'echo') {
     message.channel.send({embed: {
     color: 12887295,
     title: "Roster for Echo",
     description: "Rank 1\n\naayanna\:crown:\nycnan\:crown:\:scales:️\nAbeDaBae\:eight_spoked_asterisk:️\napiculture\nArylia\nElami\nevd\nhri\ninsertsoul\nLoonii\nqGods\nRqvine\nRzco\nultrako\nxShqde_\ngraacie"
     }});
   } else   
-    if (args == 'fbb') {
+    if (args === 'fbb') {
     message.channel.send({embed: {
     color: 12887295,
     title: "Roster for The Fresh Beat Band (FBB)",
     description: "Rank 9\n\ncutecarry\:crown:\:eight_spoked_asterisk:️\nReixnaaaa\:crown:\nausbrooks\nbeuy\nImExtra\nkillerkeem\nLizard__\notny\nUHCer\nWenesday\nwp5jdlw77dkflqks\nxEugeo\n_FBI_\nImDqnnie_\nDefinitelyLili\nhxii"
     }});
   } else
-    if (args == 'idk') {
+    if (args === 'idk') {
     message.channel.send({embed: {
     color: 12887295,
     title: "Roster for IDK",
     description: "Rank 3\n\nDEDYellowSnowTri\:crown:\nGeagleDED\:crown:\nImmortalSun\nSnowflaketimee\nVezical\nGreenio\nChanceberry\nKaiilin\nHero_Cerebrum\nHodinii\nHodarit\niMoony_DED"
     }});
   } else
-    if (args == 'kystic') {
+    if (args === 'kystic') {
     message.channel.send({embed: {
     color: 12887295,
     title: "Roster for Kystic",
     description: "Rank 8\n\nandrewew\:crown:\nDes1\:crown:\nArtxie\nHils\nlvna\nRitorugaru\nStxry\nYungEmi\n_Negqtive\nmyhku\nwallflower23"
     }});
   } else
-    if (args == 'mcthicc') {
+    if (args === 'mcthicc') {
     message.channel.send({embed: {
     color: 12887295,
     title: "Roster for MCTHICC",
     description: "Rank 10\n\nsuperbadchad\:crown:\:video_camera:\neDying\:crown:\:eight_spoked_asterisk:️\nAbqve\nBlack8787\nGcddess\neboyless\nHolyyyy\nKirstopher\nCrimsonHart\nr3i\nRoodleNoodle\nRubyZeus\nShiroSmh\nYuchithicc\nSZA\nMintNChip\nWhiteCrocs\n_Xq\nScnic\nMickeree_"
     }});
   } else
-    if (args == 'nightmare') {
+    if (args === 'nightmare') {
     message.channel.send({embed: {
     color: 12887295,
     title: "Roster for Nightmare",
     description: "Rank 13\n\nSmokeArrow\:crown:\:eight_spoked_asterisk:️\n9oneone\:crown:\nafLO\nDarkLunite\nDunkelZwerg\nBowMC\nluvTAKUMI\nMilzay\nMuMu1228\nO__w__O\nruva\ndtberg\nTwiiks\nVibrants\nWrapz\nxBorder\n_Wrathful_\n__Solid"
     }});
   } else
-    if (args == 'op') {
+    if (args === 'op') {
     message.channel.send({embed: {
     color: 12887295,
     title: "Roster for OP",
     description: "Rank 6\n\nDwagin\:crown:\nMelvinBee\:crown:\naKevin\nBquaan\nqwueen\ndqnielle\nestrallla\niCharmanderZ3\nInfiniteily\njorrt\nJvavi\nMrKitty_Cat\:bulb:\nSadJo\nMapleShade_\nvqt\:scales:️\nZurina\nScoobyDooGey\nElanei"
     }});
   } else
-    if (args == 'plutonians') {
+    if (args === 'plutonians') {
     message.channel.send({embed: {
     color: 12887295,
     title: "Roster for Plutonians",
     description: "Rank 12\n\nPhoenixHasNoAim\:crown:\nWildah\:crown:\n0lguskowy\nBrunoe101\nDSOGluttony\nFlatChest\nGandalfh\nHanyuFS\nHoekay\nAmeliaIsMyDaddy\nNexonChess\nNyctophile\nShuza\nSlasher327\nThickenings\nvnly\n_Ampera_"
     }});
   } else
-    if (args == 'velocity') {
+    if (args === 'velocity') {
     message.channel.send({embed: {
     color: 12887295,
     title: "Roster for Velocity",
