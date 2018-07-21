8f1240950019feadb01ceeba6c36bbc49b6325e0
Keiko Yukimura <41388047+Keikoooo@users.noreply.github.com>
2018-07-20 20:31:26 -0400

    Update rankings.js

--- commands/rankings.js
+++ commands/rankings.js
@@ -2,6 +2,6 @@ exports.run = (client, message, args) => {
     message.channel.send({embed: {
     color: 12887295,
     title: "Rankings",
-    description: "1. Echo\n2. Calamity\n3. IDK\n4. Velocity\n5. Artemis\n6. OP\n7. Aero\n8. Kystic\n9. Ascent\n10. McThicc\n11. BloodQuill\n12. Plutonians"
+    description: "1. Echo\n2. Calamity\n3. IDK\n4. Velocity\n5. Artemis\n6. OP\n7. Aero\n8. Kystic\n9. FBB\n10. McThicc\n11. BloodQuill\n12. Plutonians"
     }});
 }
