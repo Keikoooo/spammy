31d63a94aec4ea909833dbe13048be7271c87220
Keiko Yukimura <41388047+Keikoooo@users.noreply.github.com>
2018-07-21 14:15:17 -0400

    Create premove.js

---
+++ commands/premove.js
@@ -0,0 +1,10 @@
+exports.run = (client, message, args[0], args[1]) => {
+  const rosters = require("./roster.js");
+  let [player, team] = args;
+  
+  if (arg[1] == arg) {
+  rosters.run(client, message, args);
+  } else
+  message.channel.send(`Please insert the team's name.`);
+  }
+}
