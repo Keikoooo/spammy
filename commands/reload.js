e6eb79a5737fe7bfbfc57c6c023917318ee9f632
Keiko Yukimura <41388047+Keikoooo@users.noreply.github.com>
2018-07-21 11:40:12 -0400

    Update reload.js

--- commands/reload.js
+++ commands/reload.js
@@ -1,6 +1,11 @@
 exports.run = (client, message, args) => {
-  if(!args || args.size < 1) return message.reply("Must provide a command name to reload.");
-  // the path is relative to the *current folder*, so just ./filename.js
-  delete require.cache[require.resolve(`./${args}.js`)];
-  message.reply(`The command ${args} has been reloaded`);
+  resetBot(message.channel);
+  break;
+  
+  function resetBot(channel) {
+    // send channel a message that you're resetting bot [optional]
+    channel.send('Resetting...')
+    .then(msg => client.destroy())
+    .then(() => client.login(process.env.BOT_TOKEN));
+  }
 };
