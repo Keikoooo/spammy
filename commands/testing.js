3f5a029100e022d1f271b1513e88401afe614154
Keiko Yukimura <41388047+Keikoooo@users.noreply.github.com>
2018-07-21 17:45:55 -0400

    Update testing.js

--- commands/testing.js
+++ commands/testing.js
@@ -19,8 +19,5 @@ exports.run = (client, message, args) => {
     title: "Roster for Artemis",
     description: "Rank 5\n\n${artemisroster}\n${artemisroster}"
     }});
-  } else
-    if (args == 'bloodquill') {
-    message.channel.send("Rank 5\n\n${artemisroster}\n${artemisroster}");
   } 
 }
