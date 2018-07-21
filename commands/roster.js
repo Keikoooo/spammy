0156c2b8482b51adfba8ebe622bb5c19c673f60a
Keiko Yukimura <41388047+Keikoooo@users.noreply.github.com>
2018-07-20 20:59:53 -0400

    Update roster.js

--- commands/roster.js
+++ commands/roster.js
@@ -10,84 +10,84 @@ exports.run = (client, message, args) => {
     message.channel.send({embed: {
     color: 12887295,
     title: "Roster for Artemis",
-    description: "Rank 5\n\nmililu\nSnakeHenry\nEchanz\nFarzine\nguccitaee\nJqshZ3\nTELETUBBYJUSTIN\nkwehwy\nskkketchy\ndereknishimoto\npluv\nReachMafia\nskei\nSwipy\n_4H\n_Cath\n_Livyy_"
+    description: "Rank 5\n\nmililu\:crown:\nSnakeHenry\:crown:\nEchanz\nFarzine\nguccitaee\nJqshZ3\nTELETUBBYJUSTIN\nkwehwy\nskkketchy\ndereknishimoto\npluv\nReachMafia\nskei\nSwipy\n_4H\n_Cath\n_Livyy_"
     }});
   } else
     if (args == 'bloodquill') {
     message.channel.send({embed: {
     color: 12887295,
     title: "Roster for BloodQuill",
-    description: "Rank 11\n\nAnaworfis\nLuzifeer\njeroNV\nfindollo\nkikina01cz\nlunular\nOdran_\nPolygonWater\nXXXTentac1ion"
+    description: "Rank 11\n\nAnaworfis\:crown:\nLuzifeer\:crown:\njeroNV\nfindollo\nkikina01cz\nlunular\nOdran_\nPolygonWater\nXXXTentac1ion"
     }});
   } else
     if (args == 'calamity') {
     message.channel.send({embed: {
     color: 12887295,
     title: "Roster for Calamity",
-    description: "Rank 2\n\nCrownedVictor\nWackaroni\nAlpha_Pup10\nButterxkiller\nCaait\nJessaye\nNoobAcc\nWhatTheNut\nxGanny\nXV1etnam\nZephyrrStorm\n_Mentals\n__Double"
+    description: "Rank 2\n\nCrownedVictor\:crown:\nWackaroni\:crown:\nAlpha_Pup10\nButterxkiller\nCaait\nJessaye\nNoobAcc\nWhatTheNut\nxGanny\nXV1etnam\nZephyrrStorm\n_Mentals\n__Double"
     }});
   } else
     if (args == 'echo') {
     message.channel.send({embed: {
     color: 12887295,
     title: "Roster for Echo",
-    description: "Rank 1\n\naayanna\nycnan\nAbeDaBae\napiculture\nArylia\nElami\nevd\nhri\ninsertsoul\nLoonii\nqGods\nRqvine\nRzco\nultrako\nxShqde_\ngraacie"
+    description: "Rank 1\n\naayanna\:crown:\nycnan\:crown:\nAbeDaBae\napiculture\nArylia\nElami\nevd\nhri\ninsertsoul\nLoonii\nqGods\nRqvine\nRzco\nultrako\nxShqde_\ngraacie"
     }});
   } else   
     if (args == 'FBB') {
     message.channel.send({embed: {
     color: 12887295,
     title: "Roster for The Fresh Beat Band (FBB)",
-    description: "Rank 9\n\ncutecarry\nReixnaaaa\nausbrooks\nbeuy\nImExtra\nkillerkeem\nLizard__\nSmokeArrow\notny\nUHCer\nWenesday\nwp5jdlw77dkflqks\nxEugeo\n_FBI_\nImDqnnie_\nDefinitelyLili\nhxii"
+    description: "Rank 9\n\ncutecarry\:crown:\nReixnaaaa\:crown:\nausbrooks\nbeuy\nImExtra\nkillerkeem\nLizard__\nSmokeArrow\notny\nUHCer\nWenesday\nwp5jdlw77dkflqks\nxEugeo\n_FBI_\nImDqnnie_\nDefinitelyLili\nhxii"
     }});
   } else
     if (args == 'idk') {
     message.channel.send({embed: {
     color: 12887295,
     title: "Roster for IDK",
-    description: "Rank 3\n\nDEDYellowSnowTri\nGeagleDED\nImmortalSun\nSnowflaketimee\nVezical\nGreenio\nChanceberry\nKaiilin\nHero_Cerebrum\nHodinii\nHodarit\niMoony_DED"
+    description: "Rank 3\n\nDEDYellowSnowTri\:crown:\nGeagleDED\:crown:\nImmortalSun\nSnowflaketimee\nVezical\nGreenio\nChanceberry\nKaiilin\nHero_Cerebrum\nHodinii\nHodarit\niMoony_DED"
     }});
   } else
     if (args == 'kystic') {
     message.channel.send({embed: {
     color: 12887295,
     title: "Roster for Kystic",
-    description: "Rank 8\n\nandrewew\nBlondeAF\nArtxie\nDes1\nHils\nlvna\nRitorugaru\nSaltyAP\nStxry\nYungEmi\n_Negqtive\nmyhku\nwallflower23\neGeography"
+    description: "Rank 8\n\nandrewew\:crown:\nBlondeAF\:crown:\nArtxie\nDes1\nHils\nlvna\nRitorugaru\nSaltyAP\nStxry\nYungEmi\n_Negqtive\nmyhku\nwallflower23\neGeography"
     }});
   } else
     if (args == 'mcthicc') {
     message.channel.send({embed: {
     color: 12887295,
     title: "Roster for MCTHICC",
-    description: "Rank 10\n\nsuperbadchad\neDying\nAbqve\nBlack8787\nGcddess\neboyless\nHolyyyy\nKirstopher\nCrimsonHart\nr3i\nRoodleNoodle\nRubyZeus\nShiroSmh\nYuchithicc\nSZA\nMintNChip\nWhiteCrocs\n_Xq\nScnic\nMickeree_"
+    description: "Rank 10\n\nsuperbadchad\:crown:\neDying\:crown:\nAbqve\nBlack8787\nGcddess\neboyless\nHolyyyy\nKirstopher\nCrimsonHart\nr3i\nRoodleNoodle\nRubyZeus\nShiroSmh\nYuchithicc\nSZA\nMintNChip\nWhiteCrocs\n_Xq\nScnic\nMickeree_"
     }});
   } else
     if (args == 'Nightmare') {
     message.channel.send({embed: {
     color: 12887295,
     title: "Roster for Nightmare",
-    description: "Rank 13\n\nSmokeArrow\n9oneone\nafLO\nDarkLunite\nDunkelZwerg\nBowMC\nluvTAKUMI\nMilzay\nMuMu1228\nO__w__O\nruva\ndtberg\nTwiiks\nVibrants\nWrapz\nxBorder\n_Wrathful_\n__Solid"
+    description: "Rank 13\n\nSmokeArrow\:crown:\n9oneone\:crown:\nafLO\nDarkLunite\nDunkelZwerg\nBowMC\nluvTAKUMI\nMilzay\nMuMu1228\nO__w__O\nruva\ndtberg\nTwiiks\nVibrants\nWrapz\nxBorder\n_Wrathful_\n__Solid"
     }});
   } else
     if (args == 'op') {
     message.channel.send({embed: {
     color: 12887295,
     title: "Roster for OP",
-    description: "Rank 6\n\nDwagin\nMelvinBee\naKevin\nBquaan\nqwueen\ndqnielle\nestrallla\niCharmanderZ3\nInfiniteily\njorrt\nJvavi\nMrKitty_Cat\nSadJo\nMapleShade_\nvqt\nZurina\nScoobyDooGey\nElanei"
+    description: "Rank 6\n\nDwagin\:crown:\nMelvinBee\:crown:\naKevin\nBquaan\nqwueen\ndqnielle\nestrallla\niCharmanderZ3\nInfiniteily\njorrt\nJvavi\nMrKitty_Cat\nSadJo\nMapleShade_\nvqt\nZurina\nScoobyDooGey\nElanei"
     }});
   } else
     if (args == 'plutonians') {
     message.channel.send({embed: {
     color: 12887295,
     title: "Roster for Plutonians",
-    description: "Rank 12\n\nPhoenixHasNoAim\nWildah\n0lguskowy\nBrunoe101\nDSOGluttony\nFlatChest\nGandalfh\nHanyuFS\nHoekay\nAmeliaIsMyDaddy\nNexonChess\nNyctophile\nShuza\nSlasher327\nThickenings\nvnly\n_Ampera_"
+    description: "Rank 12\n\nPhoenixHasNoAim\:crown:\nWildah\:crown:\n0lguskowy\nBrunoe101\nDSOGluttony\nFlatChest\nGandalfh\nHanyuFS\nHoekay\nAmeliaIsMyDaddy\nNexonChess\nNyctophile\nShuza\nSlasher327\nThickenings\nvnly\n_Ampera_"
     }});
   } else
     if (args == 'velocity') {
     message.channel.send({embed: {
     color: 12887295,
     title: "Roster for Velocity",
-    description: "Rank 4\n\nIlyasey\nBavna\nAri_Eclipse\nbwear\nAstrophysicist15\nBvdie\nOofcustom\nisoar38\nits_rqse\nJesuuus\nkikidakiwi\nFornScarves\nthoester\nZCOD\n_xEmily\nYuchiFlipFlops\nAnxietyprime\nvParz\nMusicel\nArrayal"
+    description: "Rank 4\n\nIlyasey\:crown:\nBavna\:crown:\nAri_Eclipse\nbwear\nAstrophysicist15\nBvdie\nOofcustom\nisoar38\nits_rqse\nJesuuus\nkikidakiwi\nFornScarves\nthoester\nZCOD\n_xEmily\nYuchiFlipFlops\nAnxietyprime\nvParz\nMusicel\nArrayal"
     }});
   }
 }
