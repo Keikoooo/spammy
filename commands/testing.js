eee4f9297a410ef31d90a41e4a72c474de1d8f0e
Keiko Yukimura <41388047+Keikoooo@users.noreply.github.com>
2018-07-22 15:04:35 -0400

    Update testing.js

--- commands/testing.js
+++ commands/testing.js
@@ -8,14 +8,14 @@ exports.run = (client, message, args) => {
     message.channel.send({embed: {
     color: 12887295,
     title: "Roster for Aero",
-    description: "Rank 7\n\n${aeroroster[0]}\n${aeroroster[1]}\n${aeroroster[2]}"
+    description: "Rank 7\n\n${aeroroster.split(\n)}"
     }});
   } else
     if (args == 'artemis') {
     message.channel.send({embed: {
     color: 12887295,
     title: "Roster for Artemis",
-    description: "Rank 5\n\n${artemisroster[0]}\n${artemisroster[1]}\n${artemisroster[2]}"
+    description: "Rank 5\n\n${artemisroster.split(\n)"
     }});
-  }
+  } 
 }
