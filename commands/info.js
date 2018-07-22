a16eba695d5f2fe443b4d9c7cd72b7ab6748bd9d
Keiko Yukimura <41388047+Keikoooo@users.noreply.github.com>
2018-07-22 16:16:15 -0400

    Create info.js

---
+++ commands/info.js
@@ -0,0 +1,11 @@
+exports.run = (client, message, args) => {
+    let bicon = bot.user.displayAvatarURL;
+    let botembed = new Discord.RichEmbed()
+    .setDescription("Bot Information")
+    .setColor("#15f153")
+    .setThumbnail(bicon)
+    .addField("Bot Name", bot.user.username)
+    .addField("Created On", bot.user.createdAt);
+
+    return message.channel.send(botembed);
+}
