32bd12bd475037ca48a4bc73044c760aa8263b59
Keiko Yukimura <41388047+Keikoooo@users.noreply.github.com>
2018-07-22 13:57:40 -0400

    Update testing.js

--- commands/testing.js
+++ commands/testing.js
@@ -5,17 +5,9 @@ exports.run = (client, message, args) => {
     var artemisroster = ["mililu", "SnakeHenry", "Echanz"];
     
     if (args == 'aero') {
-    message.channel.send({embed: {
-    color: 12887295,
-    title: "Roster for Aero",
-    description: "${aeroroster[0]}, ${aeroroster[1]}, ${aeroroster[2]}"
-    }});
+    message.channel.send("${aeroroster[0]}, ${aeroroster[1]}, ${aeroroster[2]}");
   } else
     if (args == 'artemis') {
-    message.channel.send({embed: {
-    color: 12887295,
-    title: "Roster for Artemis",
-    description: "${artemisroster[0]}, ${artemisroster[1]}, ${artemisroster[2]}"
-    }});
+    message.channel.send("${artemisroster[0]}, ${artemisroster[1]}, ${artemisroster[2]}");
   } 
 }
