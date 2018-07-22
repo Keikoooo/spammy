2562afc9afde598c7748ae68b9478b4186679329
Keiko Yukimura <41388047+Keikoooo@users.noreply.github.com>
2018-07-21 21:02:45 -0400

    Update testing.js

--- commands/testing.js
+++ commands/testing.js
@@ -1,8 +1,10 @@
 exports.run = (client, message, args) => {
     let [add, remove, player, team] = args;
     
+    var aeroroster = ["ohjustame", "SilverSky009", "111titanic223"];
+    var artemisroster = ["mililu", "SnakeHenry", "Echanz"];
+    
     if (args == 'aero') {
-    const aeroroster = ["ohjustame", "SilverSky009", "111titanic223"];
     message.channel.send({embed: {
     color: 12887295,
     title: "Roster for Aero",
@@ -10,7 +12,6 @@ exports.run = (client, message, args) => {
     }});
   } else
     if (args == 'artemis') {
-    const artemisroster = ["mililu", "SnakeHenry", "Echanz"];
     message.channel.send({embed: {
     color: 12887295,
     title: "Roster for Artemis",
