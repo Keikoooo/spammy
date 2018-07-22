d125710d4cd4a2a7339e592aed8e2960ab74baf6
Keiko Yukimura <41388047+Keikoooo@users.noreply.github.com>
2018-07-22 16:26:47 -0400

    Update info.js

--- commands/info.js
+++ commands/info.js
@@ -1,12 +1,12 @@
 exports.run = (client, message, args) => {
-    let bicon = bot.user.displayAvatarURL;
+    let bicon = client.user.displayAvatarURL;
     let botembed = new Discord.RichEmbed()
     
     .setTitle("Bot Information")
     .setColor("#15f153")
     .setThumbnail(bicon)
-    .addField("Bot Name", bot.user.username)
-    .addField("Created On", bot.user.createdAt);
+    .addField("Bot Name", client.user.username)
+    .addField("Created On", client.user.createdAt);
 
     return message.channel.send(botembed);
 }
