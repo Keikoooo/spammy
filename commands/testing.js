ab168f37806962238c936f52bbf8ae0bdabaecd1
Keiko Yukimura <41388047+Keikoooo@users.noreply.github.com>
2018-07-21 21:00:09 -0400

    Update testing.js

--- commands/testing.js
+++ commands/testing.js
@@ -2,19 +2,19 @@ exports.run = (client, message, args) => {
     let [add, remove, player, team] = args;
     
     if (args == 'aero') {
-    const aeroroster = ["ohjustame", "SilverSky009"];
+    const aeroroster = ["ohjustame", "SilverSky009", "111titanic223"];
     message.channel.send({embed: {
     color: 12887295,
     title: "Roster for Aero",
-    description: "Rank 7\n\n${aeroroster}\n${aeroroster}"
+    description: "Rank 7\n\n${aeroroster[0]}\n${aeroroster[1]}\n${aeroroster[2]}"
     }});
   } else
     if (args == 'artemis') {
-    const artemisroster = ["mililu", "SnakeHenry"];
+    const artemisroster = ["mililu", "SnakeHenry", "Echanz"];
     message.channel.send({embed: {
     color: 12887295,
     title: "Roster for Artemis",
-    description: "Rank 5\n\n${artemisroster}\n${artemisroster}"
+    description: "Rank 5\n\n${artemisroster[0]}\n${artemisroster[1]}\n${artemisroster[2]}"
     }});
   } 
 }
