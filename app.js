515a392e0922c2c471006eb1282409b9b40ab805
Keiko Yukimura <41388047+Keikoooo@users.noreply.github.com>
2020-01-25 15:23:18 -0500

    Update app.js

--- app.js
+++ app.js
@@ -2,48 +2,29 @@ const Discord = require('discord.js');
 const request = require('request');
 const client = new Discord.Client();
 const fs = require("fs")
+const SpamMessage = "Bacon and eggs make a good breakfast, everyone."; // message you want to spam \\
+const YourDiscordID = 189544885072822272; // Replace 0 with your discord id! \\
+const prefix = ";";
 
 // const config = require("./config.json");
 
 client.on('ready', () => {
     console.log(`I am ready!`);
-    client.user.setPresence({ game: { name: `on ${client.guilds.size} servers | Keiko`, type: 0 } });
-});
-
-// This loop reads the /events/ folder and attaches each event file to the appropriate event.
-fs.readdir("./events/", (err, files) => {
-  if (err) return console.error(err);
-  files.forEach(file => {
-    let eventFunction = require(`./events/${file}`);
-    let eventName = file.split(".")[0];
-    // super-secret recipe to call events with all their proper arguments *after* the `client` var.
-    client.on(eventName, (...args) => eventFunction.run(client, ...args));
-  });
+    client.user.setPresence({ game: { name: `Pokecord | Keiko`, type: 0 } });
 });
 
 client.on('message', (message) => {
-  const prefixes = ['fel ', 'Fel ', 'FEL ', 'fEl ', 'feL ', 'fEL ', 'FeL ', 'fEL ', 'FEl '];
-  let prefix = false;
-  for(const thisPrefix of prefixes) {
-  if(message.content.startsWith(thisPrefix)) prefix = thisPrefix;
-  }
-  if(!prefix) return;  
-  
-  // Exit and stop if the prefix is not there or if user is a bot
-  if (message.author.bot) return;
-  if (message.channel.type === "dm") return; // Ignore DM channels.
-  if(message.content.indexOf(prefix) !== 0) return;
-    
-  const args = message.content.slice(prefix.length).trim().split(/ +/g);
-  const command = args.shift().toLowerCase();
-    
-  // The list of if/else is replaced with those simple 2 lines:
-  try {
-    let commandFile = require(`./commands/${command}.js`);
-    commandFile.run(client, message, args);
-  } catch (err) {
-    console.error(err);
-  }
+	if(message.author.bot) return;
+	if(!command.startsWith(prefix)) return;
+    if(command === `${prefix}spam`) {
+    	if(!message.author.id === YourDiscordID) return;
+    	let Ping = message.mentions.users.first();
+    	setInterval(function(){
+    		Ping.send(SpamMessage)
+    	},
+    		1200
+    	);
+    };
 });
 
 // THIS  MUST  BE  THIS  WAY
